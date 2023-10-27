b6c951f829a34bcc6e2bba48f169fe24912474e6
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2023-10-28 01:45:04 +0200

    Static

--- artist/settings.py
+++ artist/settings.py
@@ -140,10 +140,10 @@ STATIC_URL = '/static/'
 #STATIC_URL = 'static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'artist/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [ 
+#     os.path.join(BASE_DIR, 'artist/static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
