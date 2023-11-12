5b545fbecaa03e51ce721798b13b2e0fdd4e4560
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2023-11-12 02:08:09 +0100

    Setting Config All

--- base/urls.py
+++ base/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('payment/', views.payment_form_view, name='payment_form'),
     path('get_itunes_data/', views.get_itunes_data, name='get_itunes_data'),
     path('contact/', views.contact, name='contact_form'),
-<<<<<<< HEAD
     path('confirm/', views.confirm, name='confirm'),
-=======
->>>>>>> 2522b6aa79c2a28c557b68ecfccbb93d70f28d32
     #path('about', views.about, name='about'),
     #path('cartographie', views.cartographie, name='cartographie'),
 ]
